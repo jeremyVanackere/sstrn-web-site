b2cf0c7aba59faf6a7fe8407e853d26d680bf6b7
jeremy.vanackere <jeremy.vanackere@outlook.fr>
2021-05-10 10:35:22 +0200

    create branch

--- src/main/java/com/sstrn/sstrnwebsite/SstrnWebSiteApplication.java
+++ src/main/java/com/sstrn/sstrnwebsite/SstrnWebSiteApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SstrnWebSiteApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SstrnWebSiteApplication.class, args);
     }
-
 }
