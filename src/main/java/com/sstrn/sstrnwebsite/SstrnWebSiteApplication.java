e21ee3a88637362bd87115653dcf4607d59fe600
jeremy.vanackere <jeremy.vanackere@outlook.fr>
2021-05-10 10:33:19 +0200

    creation archi

---
+++ src/main/java/com/sstrn/sstrnwebsite/SstrnWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.sstrn.sstrnwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SstrnWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SstrnWebSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sstrn/sstrnwebsite/SstrnWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sstrn.sstrnwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SstrnWebSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
